dbf5bf99e6cead1ef74656f1211ce1503c159364
apr0032 <127146489+apr0032@users.noreply.github.com>
2023-09-26 17:24:21 +0200

    Update p2.php

--- Servidor/PHP/p2/p2.php
+++ Servidor/PHP/p2/p2.php
@@ -4,15 +4,57 @@
     </head>
     <body>
         <?php
-            
-            define("PI", 3.14);
-            $radio=6;
-
-            $volumen=(4/3)*PI*(pow($radio, 3));
-
+            // Ej 1
+            // a
+            echo "<h2>Ejercicio 1</h2>";
+            $var1 = "Hola";
+            $var2 = "Mundo";
+            $concatenar = $var1 . " " . $var2;
+            echo "1a): " . $concatenar . "<br>";
+            // b
+            $varBool = true;
+            echo "1b): " . $varBool . "<br>";
+            // c
+            $varFlo = 3.14;
+            echo "1c): " . $varFlo . "<br>";
+            // d
+            $varArray = array(
+                "valor1" => 1,
+                "valor2" => 2,
+                "valor3" => 3);
+            echo "1d): ";
+            print_r($varArray);
             echo "<br>";
 
-            echo $volumen;
+            // Ej 2
+            echo "<h2>Ejercicio 2</h2>";
+            $varBool = false;
+            echo "2): " . $varBool . "<br>";
+            echo "No muestra nada porque cuando es 'true' muestra un 1, mientras cuando es 'false' no muestra nada, por eso no apreciamos absolutamente nada en la respuesta <br>";
+
+            // Ej 3
+            echo "<h2>Ejercicio 3</h2>";
+            $concatenar = str_replace(" ", "", $concatenar); //funcion para eliminar caracter especificado
+            echo $concatenar;
+            
+            // Ej 4
+            echo "<h2>Ejercicio 4</h2>";
+            $varMs = "El operador '+' sirve para sumar el valor de variables. Con la '/'podemos dividir valores entre variables. El símbolo del dólar '$' indica que estamos utilizando variables pero no lo usaremos en las constantes o globales";
+            echo $varMs;
+
+            // Ej 5
+            echo "<h2>Ejercicio 5</h2>";
+            $long = strlen($varMs);
+            echo "Longitud de la cadena: " . $long;
+
+            // Ej 6
+            echo "<h2>Ejercicio 6</h2>";
+            
+
+
+
+            
+            
         ?>
     </body>
 </html>
