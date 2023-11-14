aefe5ddde1686f9a4543cc85be8ecb2795ef33cd
apr0032 <127146489+apr0032@users.noreply.github.com>
2023-11-14 18:51:14 +0100

    martes

--- Servidor/p4/login.php
+++ Servidor/p4/login.php
@@ -1,15 +1,21 @@
 <?php
+//
+$usuarios = array(
+    'admin' => array('contrasena' => '1234', 'rol' => 'admin'),
+    'cliente1' => array('contrasena' => '5678', 'rol' => 'cliente')
+);
+//
 // verifica inicio sesion
-if ($_POST['username'] == 'admin' && $_POST['password'] == '1234') {
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if (isset($usuarios[$username]) && $usuarios[$username]['contrasena'] == $password) {
     session_start();
-    $_SESSION['username'] = 'admin';
-    $_SESSION['login_time'] = date("Y-m-d H:i:s"); //fecha
-    header("Location: menu2.php");
+    $_SESSION['username'] = $username;
+    $_SESSION['rol'] = $usuarios[$username]['rol'];
+    $_SESSION['login_time'] = date("Y-m-d H:i:s");
+    header("Location: menu.php");
 } else {
     echo "Inicio de sesión incorrecto <a href='index.html'>Volver a intentar</a>";
 }
 ?>
-
-
-
-
