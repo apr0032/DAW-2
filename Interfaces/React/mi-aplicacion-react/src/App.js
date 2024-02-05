470267764b947321bd3eeeaa80889408a4056082
apr0032 <127146489+apr0032@users.noreply.github.com>
2024-02-05 20:29:01 +0100

    react2

--- Interfaces/React/mi-aplicacion-react/src/App.js
+++ Interfaces/React/mi-aplicacion-react/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BlackjackGame from 'BlackjackGame.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Blackjack Game</h1>
+      <BlackjackGame />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
--- Interfaces/React/mi-aplicacion-react/src/BlackjackGame.js
+++ Interfaces/React/mi-aplicacion-react/src/BlackjackGame.js
@@ -1,5 +1,3 @@
-// BlackjackGame.jsx o BlackjackGame.js
 import React, { useState, useEffect } from 'react';
 import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 
-// ... (código del juego que proporcioné anteriormente)
